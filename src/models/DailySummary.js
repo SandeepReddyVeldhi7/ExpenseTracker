a390a9dd6b3edc67e0931a3100baffcee027f2c2
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2025-07-03 23:15:19 +0530

    Update DailySummary.js

--- src/models/DailySummary.js
+++ src/models/DailySummary.js
@@ -36,6 +36,7 @@ const drinkSchema = new mongoose.Schema({
   commissionPercent: Number,
   commissionValue: Number,
   finalNetAmount: Number,
+  carryLoss: { type: Number, default: 0 },
 }, { _id: false });
 
 const dailySummarySchema = new mongoose.Schema({
