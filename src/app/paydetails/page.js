566e711db53747992145dce7aa9208e1b57101ff
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2025-07-08 16:34:14 +0530

    Update page.js

--- src/app/paydetails/page.js
+++ src/app/paydetails/page.js
@@ -195,7 +195,7 @@ export default function PayDetailsPage() {
 
 
   return (
-    <div className="max-w-7xl mx-auto p-4 sm:p-6 border border-black rounded mt-10 bg-gray-50 font-sans">
+    <div className="max-w-7xl mx-auto p-4 sm:p-6 border border-black rounded mt-10 pb-20 bg-gray-50 font-sans">
       <Toaster />
 
       <h1 className="text-xl sm:text-2xl text-black text-center font-bold mb-4">
